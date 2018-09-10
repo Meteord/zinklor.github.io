1641fef0d31f4e2d6b477c73624861869da24c91
Michael Jaumann <michael_jaumann@gmx.de>
2018-09-10 22:41:28 +0200

    CSV ready to go

--- zinklor-pages/src/App.js
+++ zinklor-pages/src/App.js
@@ -1,20 +1,61 @@
 import React, { Component } from 'react';
+import MUIDataTable from "mui-datatables";
 import logo from './logo.svg';
+import datasource from './data.csv';
+import * as d3 from 'd3';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    page: 0,
+    count: 100,
+    data: []
+  };
+
+
+  getData(source){
+      return d3.csv(source, function(data) {
+       console.log(data); 
+        return [data.einheit,data.abkürzung];
+      }).then((data) => {
+        console.log(JSON.stringify(data));
+        this.setState({ data });
+      })
+  }
+
+  componentDidMount() {
+    return this.getData(datasource);
+}
+  
+
   render() {
+    const columns = ["Einheit", "Abkürzung"];
+    const { data, page, count } = this.state;
+
+    const options = {
+      filterType: "dropdown",
+      responsive: "stacked",
+      filter: true,
+      download: false,
+      print: true,
+      count: count,
+      page: page
+    };
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Willkommen zu Zinklor</h1>
+          <h1 className="App-title">Ultimate ultimate Einheitenregister</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+      <MUIDataTable
+        data={data}
+        columns={columns}
+        options={options}
+      />
       </div>
-    );
+    );  
   }
 }
 
